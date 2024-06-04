ec61cc817a72d142c63dc9390f2ac90d2764275f
boladss <anton.boladss@gmail.com>
2024-06-05 04:07:04 +0800

    feat: add initial communication between frontend/backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,149 @@
-import { useState, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios' // for API requests
 
-function App() {
-	const [array, setArray] = useState([]);
+const flaskServer = "http://localhost:5000"
 
-  const fetchAPI = async () => {
-		const response = await axios.get("http://127.0.0.1:5000/api/test");
-		console.log(response.data);
-		setArray(response.data);
+function App() {
+	const [years, setYears] = useState([]);
+	const [selectedYear, setSelectedYear] = useState(null);
+	const [races, setRaces] = useState([]);
+	const [selectedRace, setSelectedRace] = useState(null);
+	const [drivers, setDrivers] = useState([]);
+	// const [selectedDrivers, setSelectedDrivers] = useState([]);
+	const [prediction, setPrediction] = useState(null);
+
+  const fetchYears = async () => {
+		try {
+			const response = await axios.get(`${flaskServer}/years`);
+			console.log(response.data);
+			setYears(response.data);
+		} catch (error) {
+			console.error(`Error fetching years: ${error}`);
+		}
 	}
-	
 	useEffect(() => {
-   fetchAPI();
-  }, []);
+		fetchYears()
+	}, [])
+
+	const fetchRaces = async (selectedYear) => {
+		if (selectedYear) {
+			try {
+				const response = await axios.get(`${flaskServer}/races/${selectedYear}`);
+				console.log(response.data);
+				setRaces(response.data);
+			} catch (error) {
+				console.error(`Error fetching races: ${error}`);
+			}
+		}
+	}
+	useEffect(() => {
+		fetchRaces(selectedYear)
+	}, [selectedYear])
+
+
+	const fetchDrivers = async (selectedRace) => {
+		if (selectedRace) {
+			try {
+				const response = await axios.get(`${flaskServer}/drivers/${selectedRace}`);
+				console.log(response.data);
+				setDrivers(response.data);
+			} catch (error) {
+				console.error(`Error fetching drivers: ${error}`);
+			}
+		}
+	}
+	useEffect(() => {
+		fetchDrivers(selectedRace)
+	}, [selectedRace])
+
+	const handlePredict = async() => {
+		try {
+			const response = await axios.post(`${flaskServer}/predict`, {
+				year: selectedYear,
+				race: selectedRace,
+				drivers: drivers
+			});
+			console.log("Prediction: ", response.data);
+			setPrediction(response.data);
+		}	catch (error) {
+			console.error(`Error fetching prediction: ${error}`);
+		}
+	}
+
+	// Draggable list based on Darwin Tech: https://www.youtube.com/watch?v=_nZCvxJOPwU
+	const dragDriver = useRef<number>(0);
+	const draggedOverDriver = useRef<number>(0);
+
+	function handleDriverSort() {
+		const driversClone = [...drivers];
+
+		// Swap positions
+		const temp = driversClone[dragDriver.current];
+		driversClone[dragDriver.current] = driversClone[draggedOverDriver.current];
+		driversClone[draggedOverDriver.current] = temp;
+		
+		setDrivers(driversClone);
+	}
 
   return (
     <div className="flex items-center justify-center h-screen">
-			<div className="flex-col">
-			{array.map((item, index) => (
+			<div className="flex flex-col max-w-screen-lg space-y-5">
+			{/* {years.map((item, index) => (
 				<div key={index} className="text-center text-sm p-0 m-1 bg-red-800 rounded-3xl">
 					<span>{item}</span>
 				</div>
-			))}
+			))} */}
+			<h1 className="font-black text-3xl">F1 Race Results Predictor</h1>
+				<div>
+					<label>Select Year:</label>
+					<select onChange={e => setSelectedYear(e.target.value)} className="ml-2 p-3 bg-gray-900">
+						<option value="">Select a year</option>
+						{years.map(year => (
+							<option key={year} value={year}>{year}</option>
+						))}
+					</select>
+				</div>
+
+				<div>
+					<label>Select Race:</label>
+					<select onChange={e => setSelectedRace(e.target.value)} className="ml-2 p-3 bg-gray-900">
+						<option value="">Select a race</option>
+						{races.map(race => (
+							<option key={race} value={race}>{race}</option>
+						))}
+					</select>
+				</div>
+
+				<div>
+					<label>Arrange Drivers:</label>
+					{/* <select multiple onChange={e => setSelectedDrivers([...e.target.selectedOptions].map(option => option.value))} value={selectedDrivers} className="bg-gray-900">
+						{drivers.map(driver => (
+							<option key={driver} value={driver}>{driver}</option>
+						))}
+					</select> */}
+					{drivers.map((driver, index) => (
+						<div className="relative flex rounded p-1 m-3 bg-gray-700"
+							draggable
+							onDragStart={() => (dragDriver.current = index)}
+							onDragEnter={() => (draggedOverDriver.current = index)}
+							onDragEnd={handleDriverSort}
+							onDragOver={(e) => e.preventDefault()}
+						>
+							<p>{driver}</p>
+						</div>
+					))}
+				</div>
+
+				<button onClick={handlePredict} className="flex bg-gray-900 p-3 text-xl">Predict!</button>
+				{prediction && (
+					<div>
+						<h2 className="font-bold text-xl">Prediction Results:</h2>
+						{prediction.map(driver => (
+							<option key={driver} value={driver}>{driver}</option>
+						))}
+					</div>
+				)}
 			</div>
     </div>
   )
--- flask-server/main.py
+++ flask-server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from model_test import model
 
@@ -7,11 +7,46 @@ cors = CORS(app, origins="*")
 # Enabled for all origins, change if deployed
 # Note that client-side CORS is normally disabled on Firefox
 
+years = [year for year in range(2022, 2025)]
+
+# Mock data
+races = {
+    2022: ['Race 1', 'Race 2', 'Race 3'],
+    2023: ['Race 1', 'Race 2', 'Race 3'],
+    2024: ['Race 1', 'Race 2', 'Race 3']
+}
+drivers = {
+    'Race 1': ['Driver A', 'Driver B', 'Driver C'],
+    'Race 2': ['Driver D', 'Driver E', 'Driver F'],
+    'Race 3': ['Driver G', 'Driver H', 'Driver I']
+}
+
 # Test API
-@app.route("/api/test", methods=['GET'])
+@app.route("/", methods=['GET'])
 def test():
-    
   return model()
+
+@app.route('/years', methods=['GET'])
+def get_years():
+  return jsonify(years)
+
+
+@app.route('/races/<int:year>', methods=['GET'])
+def get_races(year):
+  return jsonify(races.get(year, []))
+
+
+@app.route('/drivers/<string:race>', methods=['GET'])
+def get_drivers(race):
+  return jsonify(drivers.get(race, []))
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+  data = request.json
+  print(data['drivers'])
+  data['drivers'].sort(reverse=True)
+  return jsonify(data['drivers'])
 
 if __name__ == "__main__":
   app.run(debug=True)
