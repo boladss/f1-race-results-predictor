6f06b17c9fd072f1945e40d28b5d2e38589a85a2
boladss <anton.boladss@gmail.com>
2024-06-05 06:12:18 +0800

    feat: finalize ui interaction
    - centering a bit weird, pero okay for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,73 +4,51 @@ import axios from 'axios' // for API requests
 
 const flaskServer = "http://localhost:5000"
 
-function App() {
-	const [years, setYears] = useState([]);
-	const [selectedYear, setSelectedYear] = useState(null);
-	const [races, setRaces] = useState([]);
-	const [selectedRace, setSelectedRace] = useState(null);
-	const [drivers, setDrivers] = useState([]);
-	// const [selectedDrivers, setSelectedDrivers] = useState([]);
-	const [prediction, setPrediction] = useState(null);
+function YearSelector({years, selectedYear, setSelectedYear, setRaces, setSelectedRace, setDrivers, setPrediction}) {
+	return (
+		<div>
+			<label>Select Year:</label>
+			<select 
+				value={selectedYear || null}
+				onChange={e => {
+					setSelectedYear(parseInt(e.target.value));
+					setRaces=([]);
+					setSelectedRace(null);
+					setDrivers([]);
+					setPrediction([]);
+				}} 
+				className="ml-2 p-3 bg-gray-800">
+				<option value="">Select a year</option>
+				{years.map(year => (
+					<option key={year} value={year}>{year}</option>
+				))}
+			</select>
+		</div>
+	)
+}
 
-  const fetchYears = async () => {
-		try {
-			const response = await axios.get(`${flaskServer}/years`);
-			console.log(response.data);
-			setYears(response.data);
-		} catch (error) {
-			console.error(`Error fetching years: ${error}`);
-		}
-	}
-	useEffect(() => {
-		fetchYears()
-	}, [])
+function RaceSelector({races, selectedRace, setSelectedRace, setDrivers, setPrediction}) {
+	return (
+		<div>
+			<label>Select Race:</label>
+			<select 
+				value={selectedRace || null}
+				onChange={e => {
+					setSelectedRace(e.target.value);
+					setDrivers([]);
+					setPrediction([]);
+				}} 
+				className="ml-2 p-3 bg-gray-800">
+				<option value="">Select a race</option>
+				{races && races.map(race => (
+					<option key={race.round} value={race.round}>{race.name}</option>
+				))}
+			</select>
+		</div>
+	)
+}
 
-	const fetchRaces = async (selectedYear) => {
-		if (selectedYear) {
-			try {
-				const response = await axios.get(`${flaskServer}/races/${selectedYear}`);
-				console.log(response.data);
-				setRaces(response.data);
-			} catch (error) {
-				console.error(`Error fetching races: ${error}`);
-			}
-		}
-	}
-	useEffect(() => {
-		fetchRaces(selectedYear)
-	}, [selectedYear])
-
-
-	const fetchDrivers = async (selectedRace) => {
-		if (selectedRace) {
-			try {
-				const response = await axios.get(`${flaskServer}/drivers/${selectedRace}`);
-				console.log(response.data);
-				setDrivers(response.data);
-			} catch (error) {
-				console.error(`Error fetching drivers: ${error}`);
-			}
-		}
-	}
-	useEffect(() => {
-		fetchDrivers(selectedRace)
-	}, [selectedRace])
-
-	const handlePredict = async() => {
-		try {
-			const response = await axios.post(`${flaskServer}/predict`, {
-				year: selectedYear,
-				race: selectedRace,
-				drivers: drivers
-			});
-			console.log("Prediction: ", response.data);
-			setPrediction(response.data);
-		}	catch (error) {
-			console.error(`Error fetching prediction: ${error}`);
-		}
-	}
-
+function DriverArranger({drivers, setDrivers}) {
 	// Draggable list based on Darwin Tech: https://www.youtube.com/watch?v=_nZCvxJOPwU
 	const dragDriver = useRef<number>(0);
 	const draggedOverDriver = useRef<number>(0);
@@ -85,68 +63,139 @@ function App() {
 		
 		setDrivers(driversClone);
 	}
+	
+	return (
+		<div>
+			<h2 className="font-bold text-xl mb-2 text-center">Arrange Drivers:</h2>
+			<table>
+			{drivers && drivers.map((driver, index) => (
+				<tr className="relative flex rounded p-1 m-1 bg-gray-800"
+				draggable
+				onDragStart={() => (dragDriver.current = index)}
+				onDragEnter={() => (draggedOverDriver.current = index)}
+				onDragEnd={handleDriverSort}
+				onDragOver={(e) => e.preventDefault()}
+				>
+					<td className="flex w-7 m-0 justify-center bg-gray-700 rounded px-1" >{index+1}</td>
+					<td key={driver.driverId} value={driver.driverId} className="pl-2">{driver.name}</td>
+				</tr>
+			))}
+			</table>
+		</div>
+	)
+}
 
-  return (
-    <div className="flex items-center justify-center h-screen">
-			<div className="flex flex-col max-w-screen-lg space-y-5">
-			{/* {years.map((item, index) => (
-				<div key={index} className="text-center text-sm p-0 m-1 bg-red-800 rounded-3xl">
-					<span>{item}</span>
-				</div>
-			))} */}
-			<h1 className="font-black text-3xl">F1 Race Results Predictor</h1>
-				<div>
-					<label>Select Year:</label>
-					<select onChange={e => setSelectedYear(e.target.value)} className="ml-2 p-3 bg-gray-900">
-						<option value="">Select a year</option>
-						{years.map(year => (
-							<option key={year} value={year}>{year}</option>
-						))}
-					</select>
-				</div>
+function App() {
+	const [years, setYears] = useState([]);
+	const [selectedYear, setSelectedYear] = useState(null);
+	const [races, setRaces] = useState([]);
+	const [selectedRace, setSelectedRace] = useState(null);
+	const [drivers, setDrivers] = useState([]);
+	const [prediction, setPrediction] = useState([]);
 
-				<div>
-					<label>Select Race:</label>
-					<select onChange={e => setSelectedRace(e.target.value)} className="ml-2 p-3 bg-gray-900">
-						<option value="">Select a race</option>
-						{races.map(race => (
-							<option key={race} value={race}>{race}</option>
-						))}
-					</select>
-				</div>
+  const fetchYears = async () => {
+		try {
+			const response = await axios.get(`${flaskServer}/years`);
+			// console.log(response.data);
+			setYears(response.data);
+		} catch (error) {
+			console.error(`Error fetching years: ${error}`);
+		}
+	}
+	useEffect(() => {
+		fetchYears()
+	}, [])
 
-				<div>
-					<label>Arrange Drivers:</label>
-					{/* <select multiple onChange={e => setSelectedDrivers([...e.target.selectedOptions].map(option => option.value))} value={selectedDrivers} className="bg-gray-900">
-						{drivers.map(driver => (
-							<option key={driver} value={driver}>{driver}</option>
-						))}
-					</select> */}
-					{drivers.map((driver, index) => (
-						<div className="relative flex rounded p-1 m-3 bg-gray-700"
-							draggable
-							onDragStart={() => (dragDriver.current = index)}
-							onDragEnter={() => (draggedOverDriver.current = index)}
-							onDragEnd={handleDriverSort}
-							onDragOver={(e) => e.preventDefault()}
-						>
-							<p>{driver}</p>
+	const fetchRaces = async (selectedYear) => {
+		if (selectedYear) {
+			try {
+				const response = await axios.get(`${flaskServer}/races/${selectedYear}`);
+				// console.log(response.data);
+				setRaces(response.data);
+			} catch (error) {
+				console.error(`Error fetching races: ${error}`);
+			}
+		}
+	}
+	useEffect(() => {
+		fetchRaces(selectedYear)
+	}, [selectedYear])
+
+
+	const fetchDrivers = async (selectedYear, selectedRace) => {
+		if (selectedRace) {
+			try {
+				const response = await axios.get(`${flaskServer}/drivers/${selectedYear}/${selectedRace}`);
+				// console.log(response.data);
+				setDrivers(response.data.sort((a,b) => a.grid - b.grid)) // Ascending order
+			} catch (error) {
+				console.error(`Error fetching drivers: ${error}`);
+			}
+		}
+	}
+	useEffect(() => {
+		fetchDrivers(selectedYear, selectedRace)
+	}, [selectedYear, selectedRace])
+
+	const handlePredict = async() => {
+		try {
+			const response = await axios.post(`${flaskServer}/predict`, {
+				year: selectedYear,
+				race: selectedRace,
+				drivers: drivers
+			});
+			// console.log("Prediction: ", response.data);
+			setPrediction(response.data);
+		}	catch (error) {
+			console.error(`Error fetching prediction: ${error}`);
+		}
+	};
+
+	const handleClearInput = async() => {
+		setYears([]);
+		setSelectedYear(null);
+		setRaces([]);
+		setSelectedRace(null);
+		setDrivers([]);
+		setPrediction([]);
+		await fetchYears();
+	};
+
+  return <>
+			<div className="flex items-center justify-center m-10 space-x-10">
+				<div className="flex float-start flex-col space-y-5">
+					{/* {years.map((item, index) => (
+						<div key={index} className="text-center text-sm p-0 m-1 bg-red-800 rounded-3xl">
+						<span>{item}</span>
 						</div>
-					))}
+					))} */}
+					<h1 className="font-black text-3xl">F1 Race Results Predictor</h1>
+					<YearSelector years={years} selectedYear={selectedYear} setSelectedYear={setSelectedYear} setRaces={setRaces} setSelectedRace={setSelectedRace} setDrivers={setDrivers} setPrediction={setPrediction} />
+					<RaceSelector races={races} selectedRace={selectedRace} setSelectedRace={setSelectedRace} setDrivers={setDrivers} setPrediction={setPrediction} />			
+					<button onClick={handlePredict} disabled={!drivers.length} className="bg-gray-800 p-3 text-xl rounded-3xl">Predict!</button>		
+					<button onClick={handleClearInput} className="bg-gray-800 p-3 text-l rounded-3xl">Clear inputs</button>		
 				</div>
 
-				<button onClick={handlePredict} className="flex bg-gray-900 p-3 text-xl">Predict!</button>
-				{prediction && (
-					<div>
-						<h2 className="font-bold text-xl">Prediction Results:</h2>
-						{prediction.map(driver => (
-							<option key={driver} value={driver}>{driver}</option>
-						))}
-					</div>
-				)}
+				<div className="flex flex-row space-x-10">
+					<DriverArranger drivers={drivers} setDrivers={setDrivers} />
+
+					{prediction && (
+						<div>
+							<h2 className="font-bold text-xl mb-2 text-center">Prediction Results:</h2>
+							<table>
+								{prediction.map((driver, index) => (
+								<tr className="relative flex rounded p-1 m-1 bg-gray-800">
+									<td className="flex w-7 m-0 justify-center bg-gray-700 rounded px-1" >{index+1}</td>
+									<td key={driver.driverId} value={driver.driverId} className="pl-2">{driver.name}</td>
+								</tr>
+								))}
+							</table>
+						</div>
+					)}
+				</div>
+
 			</div>
-    </div>
-  )
+		</>
 }
 
 export default App
--- flask-server/main.py
+++ flask-server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import pandas as pd
 from model_test import model
 
 app = Flask(__name__)
@@ -7,19 +8,22 @@ cors = CORS(app, origins="*")
 # Enabled for all origins, change if deployed
 # Note that client-side CORS is normally disabled on Firefox
 
-years = [year for year in range(2022, 2025)]
+years = [year for year in range(2014, 2024)]
+drivers = pd.read_csv("assets/drivers.csv")
+races = pd.read_csv("assets/races.csv")
+results = pd.read_csv("assets/results-dataset.csv")
 
 # Mock data
-races = {
-    2022: ['Race 1', 'Race 2', 'Race 3'],
-    2023: ['Race 1', 'Race 2', 'Race 3'],
-    2024: ['Race 1', 'Race 2', 'Race 3']
-}
-drivers = {
-    'Race 1': ['Driver A', 'Driver B', 'Driver C'],
-    'Race 2': ['Driver D', 'Driver E', 'Driver F'],
-    'Race 3': ['Driver G', 'Driver H', 'Driver I']
-}
+# races = {
+#     2022: ['Race 1', 'Race 2', 'Race 3'],
+#     2023: ['Race 1', 'Race 2', 'Race 3'],
+#     2024: ['Race 1', 'Race 2', 'Race 3']
+# }
+# drivers = {
+#     'Race 1': ['Driver A', 'Driver B', 'Driver C'],
+#     'Race 2': ['Driver D', 'Driver E', 'Driver F'],
+#     'Race 3': ['Driver G', 'Driver H', 'Driver I']
+# }
 
 # Test API
 @app.route("/", methods=['GET'])
@@ -33,18 +37,29 @@ def get_years():
 
 @app.route('/races/<int:year>', methods=['GET'])
 def get_races(year):
-  return jsonify(races.get(year, []))
+  races_in_year = races.loc[races['year'] == year, ['round', 'name']]
+  races_data = races_in_year.to_dict(orient='records')
+  # print(races_data)
+  # races_data = races_in_year['name'].tolist()
+  return jsonify(races_data)
 
 
-@app.route('/drivers/<string:race>', methods=['GET'])
-def get_drivers(race):
-  return jsonify(drivers.get(race, []))
+@app.route('/drivers/<int:year>/<int:race>', methods=['GET'])
+def get_drivers(year, race):
+  drivers_in_race = results.loc[(results['year'] == year) & (results['round'] == race), ['driverId', 'grid', 'forename', 'surname']]
+  drivers_in_race['name'] = drivers_in_race['forename'] + " " + drivers_in_race['surname']
+  drivers_in_race.drop(['forename', 'surname'], axis=1, inplace=True)
+  drivers_data = drivers_in_race.to_dict(orient='records')
+  # print(drivers_data)
+  return jsonify(drivers_data)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
   data = request.json
+  print(data)
   return jsonify(data['drivers'])
+  # return jsonify(data['drivers'])
 
 if __name__ == "__main__":
   app.run(debug=True)
